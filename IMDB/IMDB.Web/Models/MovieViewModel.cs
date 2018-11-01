d3532979a69ae5f1a8cbea09ab1e84cb2025472a
Alexander Angelov <rapgeneral3@gmail.com>
2018-11-01 18:35:08 +0200

    comitting work to switch computers

---
+++ IMDB/IMDB.Web/Models/MovieViewModel.cs
@@ -0,0 +1,34 @@
+﻿using IMDB.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IMDB.Web.Models
+{
+	public class MovieViewModel
+	{
+		public MovieViewModel(Movie movie)
+		{
+			this.ID = movie.ID;
+			this.Name = movie.Name;
+			this.Score = movie.MovieScore;
+			this.Director = movie.Director.Name;
+			this.Genres = movie
+				.MovieGenres
+				.Select(x => x.Genre.GenreType)
+				.ToList();
+			this.Top5Reviews = movie
+				.Reviews
+				.OrderByDescending(r => r.ReviewScore)
+				.Take(5)
+				.Select(r => new ReviewViewModel(r))
+				.ToList();
+		}
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public double Score { get; set; }
+		public string Director { get; set; }
+		public ICollection<string> Genres { get; set; }
+		public ICollection<ReviewViewModel> Top5Reviews { get; set; }
+		public int NumberOfVotes { get; set; }
+	}
+}
---
+++ IMDB/IMDB.Web/Models/ReviewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using IMDB.Data.Models;
+
+namespace IMDB.Web.Models
+{
+	public class ReviewViewModel
+	{
+		public ReviewViewModel(Review review)
+		{
+			this.ReviewID = review.ID;
+			this.Rating = review.MovieRating;
+			this.Score = review.ReviewScore;
+			this.Text = review.Text;
+			this.User = review.User.Email;
+			this.UserID = review.User.Id;
+			this.MovieName = review.Movie.Name;
+			this.NumberOfVotes = review.NumberOfVotes;
+		}
+		public int ReviewID { get; set; }
+		public double Rating { get; set; }
+		public double Score { get; set; }
+		public string Text { get; set; }
+		public string User { get; set; }
+		public string UserID { get; set; }
+		public string MovieName { get; set; }
+		public int NumberOfVotes { get; set; }
+	}
+}
